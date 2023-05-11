1f84bf4bff8e4e7798907d782fd4af98aa95228e
Lynczera <leonardochen.code@gmail.com>
2023-05-10 19:15:42 -0600

    try fix pdf view

--- src/components/CV.js
+++ src/components/CV.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import "react-pdf/dist/esm/Page/AnnotationLayer.css"
+import resume from '../images/Leonardo_Chen_Resume.pdf'
 export const CV = () => {
     return (
         <div className='resumeContainer'>   
-        <a className='downloadResumeBtn'  href={require('../images/Leonardo_Chen_Resume.pdf')} download="Resume" target='_blank'> Download resume</a>
-        <Document file = {require('../images/Leonardo_Chen_Resume.pdf')}>
+        <a className='downloadResumeBtn'  href={resume} download="Resume" target='_blank'> Download resume</a>
+        <Document file = {resume}>
         <Page pageNumber={1} renderTextLayer={false}/>
       </Document>
 
