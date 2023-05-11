02a5922120b6d2a1768b3810a63516e2044c4972
Lynczera <leonardochen.code@gmail.com>
2023-05-10 19:41:27 -0600

    fixed pdf

--- src/components/CV.js
+++ src/components/CV.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
 import "react-pdf/dist/esm/Page/AnnotationLayer.css"
 import resume from '../images/Leonardo_Chen_Resume.pdf'
 export const CV = () => {
     return (
         <div className='resumeContainer'>   
         <a className='downloadResumeBtn'  href={resume} download="Resume" target='_blank'> Download resume</a>
-        <Document file = {resume}>
+        <img className='resumeImg' src={require("../images/Leonardo_Chen_Resume.png")}/> 
+        
+        {/* <Document file = {resume}>
         <Page pageNumber={1} renderTextLayer={false}/>
-      </Document>
+      </Document> */}
 
       
         </div>
